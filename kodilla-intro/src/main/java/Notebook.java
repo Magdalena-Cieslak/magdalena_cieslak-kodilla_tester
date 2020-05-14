f049829f37c0358e6ddd9fd8e19feea721d50af8
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-05-14 22:27:24 +0200

    module2.3

---
+++ kodilla-intro/src/main/java/Notebook.java
@@ -0,0 +1,41 @@
+public class Notebook {
+    int weight;
+    int price;
+    int year;
+
+    public Notebook(int weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+    }
+
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is cheap.");
+        } else if (this.price > 600 && this.price <= 1000) {
+            System.out.println("The price is good.");
+        } else {
+            System.out.println("This notebook is expencive.");
+        }
+    }
+
+    public void checkWeight() {
+        if (this.weight <= 600) {
+            System.out.println("This notebook is light");
+        } else if (this.weight > 600 && this.weight <= 1600) {
+            System.out.println("This notebook is not too heavy");
+        } else {
+            System.out.println("This notebook is heavy");
+        }
+    }
+
+    public void checkYearAndPrice() {
+        if (this.year > 2000 && this.price <= 1000) {
+            System.out.println("This notebook is recomended.");
+        } else if (this.year <= 2000 && this.price > 1000) {
+            System.out.println("This notebook is acceptable.");
+        } else {
+            System.out.println("This notebook is not recomended.");
+        }
+    }
+}
