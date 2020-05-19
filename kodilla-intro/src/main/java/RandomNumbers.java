07d7c9bbad2101e693eca429673698e25c96423a
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-05-19 21:51:08 +0200

    2.5 poprawione

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -8,11 +8,27 @@ public class RandomNumbers {
     static void usingRandomClass() {
 
         Random randomGenerator = new Random();
-        int randomInt = randomGenerator.nextInt(30) + 1;
-
-        int a = 0;
-        while ((a + randomInt) <= 5000) {
+        int sum = 0;
+        while (sum <= 5000) {
             int randomInt = randomGenerator.nextInt(30) + 1;
+            sum = sum + randomInt;
+            System.out.println(sum);
+        }
+    }
+
+    static int returnMaxNumber(int max, int randomInt) {
+        int max <=30;
+        if (max >= randomInt) {
+            return max;
+        }
+    }
+
+    static int returnMinNumber(int min, int randomInt) {
+        int min >=0;
+        int min <30;
+        if (min <= randomInt) {
+            return min;
         }
     }
 }
+
