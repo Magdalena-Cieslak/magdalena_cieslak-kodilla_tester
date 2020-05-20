c7266f037af59f9ab8b49225b0cab92eb24e9a2e
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-05-20 10:38:44 +0200

    2.8

---
+++ kodilla-intro/src/main/java/Colors.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Colors {
+
+    public static String getFirstLetterOfColor() {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String letter = scanner.nextLine().trim();
+            if (letter.length() = 1) {
+                return letter;
+            }
+            System.out.println("It's not correct. Try again.");
+        }
+    }
+    public static String getUserSelection() {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Select first letter of color (W-white, B-black, R-red, Y-yellow):");
+            String letter = scanner.nextLine().trim().toUpperCase();
+            switch (letter) {
+                case "W" :
+                    System.out.println("Your color is white.");
+                    break;
+                case "B" :
+                    System.out.println("Your color is black.");
+                    break
+                case "R" :
+                    System.out.println("Your color is red.");
+                    break;
+                case "Y" :
+                    System.out.println("Your color is yellow.");
+                    break;
+                default:
+                    System.out.println("Wrong letter, try again.");
+            }
+        }
+    }
+}
