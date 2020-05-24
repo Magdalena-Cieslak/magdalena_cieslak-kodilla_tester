9b19109db96180b183950ad84495e37142b435ad
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-05-24 13:48:08 +0200

    moduł 3.5 - uzupełnienie

--- kodilla-basic-test/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-test/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -1,4 +1,19 @@
 package com.kodilla.basic_assertion;
 
 public class Calculator {
+
+    public int sumNumbers(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public int subtractNumbers(int a, int b) {
+        int subtract = a - b;
+        return subtract;
+    }
+
+    public int square (int a) {
+        int square = a * a;
+        return square;
+    }
 }
