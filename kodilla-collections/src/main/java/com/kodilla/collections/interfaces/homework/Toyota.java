722705380b3d9371b8a3c819bf5f9acc4a22fb34
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-05-29 18:36:31 +0200

    Zadanie 4.1

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Toyota.java
@@ -0,0 +1,24 @@
+package com.kodilla.collections.interfaces.homework;
+
+public class Toyota implements Car {
+    private int speed;
+
+    public Toyota(int speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public int getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public int increaseSpeed() {
+        return (speed + 70);
+    }
+
+    @Override
+    public int decreaseSpeed() {
+        return speed - 50;
+    }
+}
