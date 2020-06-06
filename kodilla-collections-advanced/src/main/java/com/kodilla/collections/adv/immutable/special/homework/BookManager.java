2ec41726b2722d3834377978d061edbe8f6f9459
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-06 17:47:48 +0200

    Zadanie 5.4 - poprawki_2

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -1,18 +1,22 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
+import javax.crypto.spec.PSource;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BookManager {
 
-    Map<String, String> library = new HashMap<>();
+    Map<Book, Book> library = new HashMap<>();
 
     public Book createBook(String title, String author) {
         Book book = new Book(title, author);
-        library.put(title, author);
 
-        for(Map.Entry<String, String> note : library.entrySet()) {
-            System.out.println("Author: " + note.getValue() + ", Title: " + note.getKey());
+        for (Map.Entry<Book, Book> note : library.entrySet()) {
+            if ((library.get(title)).equals(book.getAuthor())) {
+                return book;
+            } else {
+                library.put(book.getTitle(), book.getAuthor());
+            }
         }
         return book;
     }
