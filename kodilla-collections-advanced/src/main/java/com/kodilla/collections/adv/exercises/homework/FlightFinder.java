45df7a24d87ae43eff6bfafd4d208bbe2d7f337f
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-10 19:24:43 +0200

    Zadanie 5.5 - poprawki

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
@@ -3,14 +3,14 @@ package com.kodilla.collections.adv.exercises.homework;
 import java.util.*;
 
 public class FlightFinder {
-
-    private Map<List<Flight>, List<Flight>> towns = new HashMap<>();
+    Map<String, String> towns = new HashMap<>();
 
     public List<Flight> findFlightFrom (String departure) {
-        return towns.get(departure);
-    }
-
-    public List<Flight> findFlightsTo (String arrival) {
-        return towns.get(arrival);
+        for(Map.Entry<String, String> note : towns.entrySet()) {
+            if(note.equals(departure)){
+                towns.get(note);
+            }
+            System.out.println("Lot na trasie: " + departure + " - " + note);
+        }
     }
 }
