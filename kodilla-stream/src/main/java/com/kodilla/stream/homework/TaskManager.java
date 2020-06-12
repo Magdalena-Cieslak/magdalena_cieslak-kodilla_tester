e8227a1e0dda748bb91026a2d28443b5fc5c5b4f
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-12 17:32:03 +0200

    Zadanie 6.1 - poprawki

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
@@ -2,15 +2,16 @@ package com.kodilla.stream.homework;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TaskManager {
     public static void main(String[] args) {
-        LocalDate currentDate = LocalDate.now();
 
-        TaskRepository.getTasks()
+        List<LocalDate> deadlinesDate = TaskRepository.getTasks()
                 .stream()
-                .filter(d -> d.getDeadline().equals(currentDate))
-                .map(dd -> dd.getName())
-                .forEach(dd -> System.out.println(dd));
+                .filter(d -> d.getDeadline().equals(LocalDate.now()))
+                .map(dd -> dd.getDeadline())
+                .collect(Collectors.toList());
+        System.out.println(deadlinesDate);
     }
 }
