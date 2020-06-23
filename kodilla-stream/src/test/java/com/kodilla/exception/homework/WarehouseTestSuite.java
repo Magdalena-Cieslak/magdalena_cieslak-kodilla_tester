c3662ba2f43076f3c9bf99e5752d43d327f463bb
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-23 18:47:02 +0200

    TAsk 6.5 correction_1

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -5,17 +5,38 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WarehouseTestSuite {
+
     @Test
     public void getOrder() throws OrderDoesntExistException {
 
+        //given
         Warehouse warehouse = new Warehouse();
-
         Order one = new Order("one");
         Order two = new Order("two");
         warehouse.addOrder(one);
         warehouse.addOrder(two);
 
-        Order res = warehouse.getOrder("two");
-        assertEquals(two, res);
+        //when
+        Order result = warehouse.getOrder("two");
+
+        //then
+        assertEquals(two, result);
+    }
+
+    @Test
+        public void getOrder_withException() throws OrderDoesntExistException {
+
+        //given
+        Warehouse warehouse = new Warehouse();
+        Order one = new Order("one");
+        Order two = new Order("two");
+        warehouse.addOrder(one);
+        warehouse.addOrder(two);
+
+        //when
+        Order result = warehouse.getOrder("three");
+
+        //then
+        assertEquals(expected, result);
     }
 }
