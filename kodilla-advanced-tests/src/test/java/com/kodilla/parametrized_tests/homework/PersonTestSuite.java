aed17c415620939ab516759bc74a6d9879eea312
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-23 22:17:56 +0200

    task 7.3 - correction_1

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -7,20 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonTestSuite {
 
-    Person person = new Person(double height, double weight);
+    Person person_1 = new Person(1.65, 85);
+    Person person_2 = new Person(1.85, 90);
+    Person person_3 = new Person(1.2, 20);
+    Person person_4 = new Person(0.0, 35);
 
     @ParameterizedTest
     @MethodSource(value = "com.kodilla.parametrized_tests.homework.DoubleSources#provideDoubleForTestingBMI")
-    public void shouldCalculateBmi(double input, double expected) {
-        assertEquals(expected, person.getBMI());
+    public void shouldCalculateBmi(double input, double expected, double delta) {
+
+        assertEquals(expected, person_1.getBMI());
     }
 
     @ParameterizedTest
     @MethodSource(value = "com.kodilla.parametrized_tests.homework.DoubleSourcesWithZero#provideDoubleForTestingBMI")
     public void shouldNotCalculateBmiWhenHeightIsZero(double input, double expected) {
-        assertEquals(expected, person.getBMI());
+        assertEquals(expected, person_4.getBMI());
+
     }
-
-
-
 }
