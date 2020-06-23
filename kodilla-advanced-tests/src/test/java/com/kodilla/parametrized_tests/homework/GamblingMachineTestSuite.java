6af09bef0ccb7943210cf66112804e1d6e60ae89
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-23 21:55:17 +0200

    task 7.2

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class GamblingMachineTestSuite {
+
+    private GamblingMachine gamblingMachine = new GamblingMachine();
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "/intNumbersSources.csv", numLinesToSkip = 1)
+        public void shouldThrowExceptionWhenNumbersAreDifferentFromSix(int input)  {
+
+        }
+
+    }
