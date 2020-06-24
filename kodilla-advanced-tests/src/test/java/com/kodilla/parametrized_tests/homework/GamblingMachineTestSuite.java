9ee9f5b12da505edd3b7cca3bd92c73b605dddc8
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-24 15:45:07 +0200

    task 7.2 - correction_1

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -3,14 +3,33 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.util.Set;
+
 class GamblingMachineTestSuite {
 
     private GamblingMachine gamblingMachine = new GamblingMachine();
 
-        @ParameterizedTest
-        @CsvFileSource(resources = "/intNumbersSources.csv", numLinesToSkip = 1)
-        public void shouldThrowExceptionWhenNumbersAreDifferentFromSix(int input)  {
-
-        }
-
+    @ParameterizedTest(expected = InvalidNumbersException.class)
+    @CsvFileSource(resources = "/intNumbersSources_GreaterThanSix.csv")
+    public void shouldThrowExceptionWhenNumberOfNumbersIsGreaterThanSix(Set<Integer> input) throws InvalidNumbersException {
+        gamblingMachine.howManyWins(input);
     }
+
+    @ParameterizedTest(expected = InvalidNumbersException.class)
+    @CsvFileSource(resources = "/intNumbersSources_LessThanSix.csv")
+    public void shouldThrowExceptionWhenNumberOfNumbersIsLessThanSix(Set<Integer> input) throws InvalidNumbersException {
+        gamblingMachine.howManyWins(input);
+    }
+
+    @ParameterizedTest(expected = InvalidNumbersException.class)
+    @CsvFileSource(resources = "/intNumbersSources_numbersLessOne.csv")
+    public void shouldThrowExceptionWhenNumbersAreLessThanOne(Set<Integer> input) throws InvalidNumbersException {
+        gamblingMachine.howManyWins(input);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/intNumbersSources_correct.csv")
+    public void shouldCalculateHowManyWins(Set<Integer> input) throws InvalidNumbersException {
+        gamblingMachine.howManyWins(input);
+    }
+}
