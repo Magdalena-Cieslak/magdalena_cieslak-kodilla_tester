894912067aed6af96ff820f69c413f817bc756f6
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-25 07:55:28 +0200

    task 7.3 - correction_6

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/DoubleSourcesWithZero.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/DoubleSourcesWithZero.java
@@ -8,8 +8,8 @@ public class DoubleSourcesWithZero {
 
     static Stream<Arguments> provideDoubleWithZeroForTestingBMI() {
         return Stream.of(
-                Arguments.of(0.0, 65.0, "Height is not correct, try again"),
-                Arguments.of(0.0, 75.0, "Height is not correct, try again")
+                Arguments.of(0.0, 65.0, 0.0),
+                Arguments.of(0.0, 75.0, 0.0)
         );
     }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -17,9 +17,9 @@ class PersonTestSuite {
 
     @ParameterizedTest
     @MethodSource(value = "com.kodilla.parametrized_tests.homework.DoubleSourcesWithZero#provideDoubleWithZeroForTestingBMI")
-    public void shouldNotCalculateBmiWhenHeightIsZero(double height, double weight, String expected) {
+    public void shouldNotCalculateBmiWhenHeightIsZero(double height, double weight, double expected) {
         Person person = new Person(height, weight);
 
-        assertEquals(expected, person.getBMI());
+        assertEquals(expected, Double.parseDouble(person.getBMI()), 0.001);
     }
 }
