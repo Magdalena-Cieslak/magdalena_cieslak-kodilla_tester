a5f8051b668e355756638288aca1107cabacfd44
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-29 18:11:01 +0200

    Task 7.6 - correction_1

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherNotificationService.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherNotificationService.java
@@ -1,24 +1,29 @@
 package com.kodilla.mockito.homework;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WeatherNotificationService {
 
-    private Map<WeatherClient, WeatherNotification> weatherClients = new HashMap<>();
+    private Map<WeatherNotification, List<WeatherClient>> weatherNotifications = new HashMap<>();
+    private List<WeatherClient> weatherClients = new ArrayList<>();
 
-    public void addSubscriber(WeatherClient weatherClient, WeatherNotification weatherNotification) {
-        this.weatherClients.put(weatherClient,weatherNotification);
+    public void addSubscriber(WeatherClient weatherClient) {
+        this.weatherClients.add(weatherClient);
     }
 
-    public void sendNotification(WeatherClient weatherClient, WeatherNotification weatherNotification) {
-        this.weatherClients.forEach(WeatherClient::receive);
+    public void sendNotification(WeatherNotification weatherNotification, List<WeatherClient> weatherClient) {
+        this.weatherClients.forEach(weatherClient -> weatherClient.receive(weatherNotification));
     }
 
     public void removeNotification(WeatherClient weatherClient, WeatherNotification weatherNotification) {
+        this.weatherClients.remove(weatherNotification);
     }
 
     public void removeSubscriber(WeatherClient weatherClient, WeatherNotification weatherNotification) {
+        this.weatherClients.remove(weatherClient);
     }
 }
 
