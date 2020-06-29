99b045edf40cb67b37eca922ba2d44bcd6045485
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-06-29 20:43:39 +0200

    Task 7.6 - correction_2

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherNotificationService.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherNotificationService.java
@@ -1,28 +1,28 @@
 package com.kodilla.mockito.homework;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class WeatherNotificationService {
 
-    private Map<WeatherNotification, List<WeatherClient>> weatherNotifications = new HashMap<>();
-    private List<WeatherClient> weatherClients = new ArrayList<>();
+    private Map<WeatherNotification, List<WeatherClient>> weatherClients = new HashMap<>();
 
-    public void addSubscriber(WeatherClient weatherClient) {
-        this.weatherClients.add(weatherClient);
+    public void addSubscriber(WeatherNotification weatherNotification, List<WeatherClient> weatherClient) {
+        this.weatherClients.put(weatherNotification, weatherClient);
     }
 
-    public void sendNotification(WeatherNotification weatherNotification, List<WeatherClient> weatherClient) {
-        this.weatherClients.forEach(weatherClient -> weatherClient.receive(weatherNotification));
+    public void sendNotification(WeatherNotification weatherNotification, WeatherClient weatherClient) {
+        this.weatherClients.forEach(weatherClient -> {
+            weatherClient.receive(weatherNotification);
+        });
     }
 
-    public void removeNotification(WeatherClient weatherClient, WeatherNotification weatherNotification) {
+    public void removeNotification(WeatherNotification weatherNotification) {
         this.weatherClients.remove(weatherNotification);
     }
 
-    public void removeSubscriber(WeatherClient weatherClient, WeatherNotification weatherNotification) {
+    public void removeSubscriber(WeatherClient weatherClient) {
         this.weatherClients.remove(weatherClient);
     }
 }
