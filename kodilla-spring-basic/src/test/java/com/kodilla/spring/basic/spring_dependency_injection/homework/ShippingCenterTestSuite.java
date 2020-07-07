d9365c1c256671c7775c08b522759f5c001206af
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-07-07 13:50:24 +0200

    task 9.3

---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenterTestSuite.java
@@ -0,0 +1,20 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class ShippingCenterTestSuite {
+
+    @Test
+    public void shouldSendPackage() {
+        ApplicationContext context = new AnnotationConfigApplicationContext();
+        DeliveryService bean = context.getBean(DeliveryService.class);
+        boolean value = bean.deliveryPackage("NY City", 20.5);
+        Assertions.assertTrue(true);
+    }
+
+}
