1c43e9c62c2c23614d206634b99d11f42dc91a2c
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-07-08 13:35:14 +0200

    SpringBasicRunner

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/SpringBasicRunner.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/SpringBasicRunner.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBasicRunner {
-
     public static void main(String[] args) {
 
     }
