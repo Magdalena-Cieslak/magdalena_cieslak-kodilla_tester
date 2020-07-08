3a5db0852d5aafa981aed09a767a9a7bca6de442
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-07-08 17:49:22 +0200

    task 9.4

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homewerk/Car.java
@@ -0,0 +1,8 @@
+package com.kodilla.spring.basic.spring_configuration.homewerk;
+
+public interface Car {
+
+    boolean hasHeadlightsTurnedOn(boolean lights);
+
+    String getCarType();
+}
---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homewerk/CarFactory.java
@@ -0,0 +1,38 @@
+package com.kodilla.spring.basic.spring_configuration.homewerk;
+
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class CarFactory {
+
+    @Bean
+    public Car chooseCarDependingOnDate(LocalDate localDate) {
+        Car car;
+        localDate = LocalDate.now();
+
+        if(localDate.isAfter(LocalDate.of(2020, 6, 20)) &&
+        localDate.isBefore(LocalDate.of(2020, 9, 22))) {
+            car = new Cabrio();
+        } else if(localDate.isAfter(LocalDate.of(2020, 12, 21)) &&
+        localDate.isBefore(localDate.of(2021, 03, 20))) {
+            car = new SUV();
+        } else {
+            car = new Sedan();
+        }
+        return car;
+    }
+
+    public void turnOnTheLights(LocalTime localTime) {
+        boolean lights;
+        localTime = LocalTime.now();
+
+        if(localTime.isAfter(LocalTime.of(20, 00)) &&
+                localTime.isBefore(localTime.of(6, 00))) {
+            lights = true;
+        } else {
+            lights = false;
+        }
+    }
+}
