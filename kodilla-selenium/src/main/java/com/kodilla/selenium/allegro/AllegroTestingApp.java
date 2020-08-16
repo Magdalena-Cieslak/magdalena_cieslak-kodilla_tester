136bbd2c033e38673f397fd6d95cf85bdfb525be
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-08-16 10:01:05 +0200

    Task 13.5

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -12,11 +14,14 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl");
 
-        while (!driver.findElement(By.xpath("//html/body/div/div/div/div/div")).isDisplayed()) {
-        }
-        driver.findElement(By.xpath("//html/body/div/div/div/div/div/div[2]/button[2]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+//        wait.until(ExpectedConditions.alertIsPresent());
 
-        WebElement inputField = driver.findElement(By.xpath("//div[@data-box-name=\"allegro.metrumHeader.search\"]/div/form/input"));
+        wait.until(driver.findElement(By.xpath("//html/body/div/div/div/div/div")).isDisplayed());
+        driver.findElement(By.xpath("//html/body/div/div/div/div/div/div[2]/button[2]")).click();
+        wait.until(!driver.findElement(By.xpath("//html/body/div/div/div/div/div")).isDisplayed());
+
+        WebElement inputField = driver.findElement(By.cssSelector(".mp4t_8 mp4t_0_m mryx_8 mryx_0_s m7er_k4 m7er_k4 m7er_wn _fee54_02svT"));
         inputField.sendKeys("mavic mini");
 
         WebElement categoryDropdown = driver.findElement(By.xpath("/html/body/div/div/header/div/div/div/div/form/div[2]/div/select"));
