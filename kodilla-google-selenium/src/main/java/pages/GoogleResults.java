b427b0c840b0cb9aaa148feffdf44849c4c93ebf
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-08-18 11:41:27 +0200

    Task 13.6

--- kodilla-google-selenium/src/main/java/pages/GoogleResults.java
+++ kodilla-google-selenium/src/main/java/pages/GoogleResults.java
@@ -4,13 +4,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.Random;
 
 public class GoogleResults extends AbstractPage {
 
     @FindBy(css = "div[class='g']")
     private List<WebElement> results;
+
+
+    public GoogleResults openRandomResult() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        Random random = new Random();
+        int drawnIndex = random.nextInt(6);
+
+        wait.until(ExpectedConditions.elementToBeClickable(results.get(drawnIndex))).click();
+        GoogleResults googleResults = new GoogleResults(driver);
+        return googleResults;
+    }
 
     public GoogleResults(WebDriver driver) {
         super(driver);
--- kodilla-google-selenium/src/main/java/pages/GoogleSearch.java
+++ kodilla-google-selenium/src/main/java/pages/GoogleSearch.java
@@ -2,7 +2,6 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -15,7 +14,7 @@ public class GoogleSearch extends AbstractPage {
     @FindBy(css = "input[title='Szukaj']")
     static WebElement inputField;
 
-    @FindBy(css = "input[value='Google Szukaj']")
+    @FindBy(css = "input[value='Szukaj w Google']")
     static List<WebElement> searchButton;
     private static GoogleResults googleResults;
 
@@ -30,7 +29,7 @@ public class GoogleSearch extends AbstractPage {
         googleResults.iSeeResults();
     }
 
-    public GoogleResults loadResults(WebDriver driver){
+    public GoogleResults loadResults(WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.elementToBeClickable(searchButton.get(0))).click();
         GoogleResults googleResults = new GoogleResults(driver);
