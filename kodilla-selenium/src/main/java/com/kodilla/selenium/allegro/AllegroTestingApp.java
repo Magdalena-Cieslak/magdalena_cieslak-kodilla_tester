cc94452bc290f57032c8093970b3046ff0aca8ac
Magdalena Cieslak <magdalena.maria.cieslak@gmail.com>
2020-08-18 21:12:52 +0200

    Task 13.5 - correction 1

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -14,21 +14,25 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl");
 
-//        WebDriverWait wait = new WebDriverWait(driver, 5);
-//        wait.until(ExpectedConditions.alertIsPresent());
-
-        while (!driver.findElement(By.xpath("//html/body/div/div/div/div/div")).isDisplayed()) {
-        }
+        wait(driver);
         driver.findElement(By.xpath("//html/body/div/div/div/div/div/div[2]/button[2]")).click();
 
-        WebElement inputField = driver.findElement(By.xpath("//html:body/div/div/header/div/div/div/div/form/input"));
+        WebElement inputField = driver.findElement(By.cssSelector("div > form > input"));
         inputField.sendKeys("mavic mini");
 
-        WebElement categoryDropdown = driver.findElement(By.xpath("//html/body/div/div/header/div/div/div/div/form/div[2]/div/select"));
+        WebElement categoryDropdown = driver.findElement(By.cssSelector("form > div > div > select"));
         Select categorySelect = new Select(categoryDropdown);
         categorySelect.selectByIndex(3);
 
-        inputField = driver.findElement(By.xpath("//div[@data-box-name=\"allegro.metrumHeader.search\"]/div/form/button"));
+        inputField = driver.findElement(By.cssSelector("form > button"));
         inputField.submit();
+
+        List<WebElement> elements = driver.findElements(By.cssSelector("._9c44d_1V-js"));
+        System.out.println(elements.get(0).getText());
+    }
+
+    private static void wait(WebDriver driver) {
+        while (!driver.findElement(By.xpath("//html/body/div[3]/div[8]/div/div[2]/div")).isDisplayed()) {
+        }
     }
 }
